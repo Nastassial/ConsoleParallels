9110cb490b73df7f347ad29d7a397da25613325e
Nastassial <nastassialmezhen@gmail.com>
2023-11-19 11:38:50 +0300

    Fixed bugs

--- ConsoleParallels/Program.cs
+++ ConsoleParallels/Program.cs
@@ -1,41 +1,44 @@
 ﻿using System.Diagnostics;
 
-int tasksCount = 10, startIndex = 0, endIndex = 0, subArrayLength;
-long resultSum = 0;
 
 int[] numbers = new int[10_000_000];
 
 Random random = new Random();
-
-Stopwatch sw = new Stopwatch();
-
 
 for (int i = 0; i < numbers.Length; i++)
 {
     numbers[i] = random.Next(-10_000, 10_001);
 }
 
-subArrayLength = (int)Math.Ceiling(numbers.Length / (decimal)tasksCount);
+int tasksCount = 10;
+int subArrayLength = (int)Math.Ceiling(numbers.Length / (decimal)tasksCount);
 
 Task<long>[] tasks = new Task<long>[tasksCount];
+
+Stopwatch sw = new Stopwatch();
 
 sw.Start();
 
 for (int i = 0; i < tasks.Length; i++)
 {
-    startIndex = endIndex;
-    endIndex = (i == tasks.Length - 1) ? numbers.Length : startIndex + subArrayLength;
+    int startIndex = i * subArrayLength;
+    int endIndex = (i == tasks.Length - 1) ? numbers.Length : startIndex + subArrayLength;
 
     tasks[i] = Task.Run(() => Sum(numbers, startIndex, endIndex));
-
-    resultSum += tasks[i].Result;
 }
 
-Console.WriteLine("resultSum = " + resultSum);
+Task.WaitAll(tasks);
+
+long resultSum = 0;
+
+foreach (var task in tasks)
+{
+    resultSum += task.Result;
+}
 
 sw.Stop();
 
-Console.WriteLine(sw.ElapsedMilliseconds);
+Console.WriteLine($"TotalSum: {resultSum}\nTime: {sw.ElapsedMilliseconds} ms");
 
 
 long Sum(int[] numbers, int startIndex, int endIndex)
